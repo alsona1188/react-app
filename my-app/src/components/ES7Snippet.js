9babab384f8738b047a37751f61ffcae5bd38e9c
Alsona Natsi <alsona.naci1188@gmail.com>
2024-07-13 22:07:11 +0000

    Snippet ES7

---
+++ my-app/src/components/ES7Snippet.js
@@ -0,0 +1,6 @@
+// rfce => Create a React Function
+// rafce => Create a React Arrow Function
+// rce => Create a React Class Export
+// rconst => Create a React Constructor
+// clg => console.log('object')
+// clo => Console.log(name + object)
